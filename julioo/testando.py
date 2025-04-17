224992361546c751285414c728d9a840b84e8b1d
Rafael de Gino Barbosa <134007361+rafael-gino2@users.noreply.github.com>
2025-04-16 21:49:43 -0300

    Add files via upload

---
+++ julioo/testando.py
@@ -0,0 +1,11 @@
+#Atividade 3 do Julio PROVA 
+uso_cpu = [
+[20, 25, 40, 50, 45, 60, 55, 35, 70, 65],
+[30, 35, 50, 60, 40, 55, 60, 45, 50, 55],
+[15, 20, 30, 25, 35, 40, 45, 50, 55, 60],
+[10, 15, 25, 35, 45, 50, 55, 60, 65, 70],
+]
+def dados_crescente(uso_cpu):
+  crescente2 = sorted(uso_cpu)
+  return crescente2
+dados_crescente(uso_cpu)
